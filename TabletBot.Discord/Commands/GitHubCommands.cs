40a03bb3ea18628d143b93e3b06bb6f0a4d52b8f
InfinityGhost <seelkadoom36@gmail.com>
2019-12-25 23:50:13 -0500

    Add getartifacts command

--- TabletBot.Discord/Commands/GitHubCommands.cs
+++ TabletBot.Discord/Commands/GitHubCommands.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -13,6 +16,8 @@ namespace TabletBot.Discord.Commands
         private GitHubAPI GitHub => TabletBot.GitHub.GitHubAPI.Current;
         private const string RepositoryOwner = "InfinityGhost";
         private const string RepositoryName = "OpenTabletDriver";
+        private static readonly Regex ArtifactRegex = new Regex("<.+?href=\"/InfinityGhost/OpenTabletDriver/suites/(?<Suite>.+?)/artifacts/(?<Artifact>.+?)\">(?<Name>.+?)</.+?>");
+        private static readonly Regex CommitRegex = new Regex("href=\".+?commit/(?<SHA>.+?)\"");
 
         [Command("overview", RunMode = RunMode.Async), Alias("info")]
         public async Task GetRepositoryOverview()
@@ -86,6 +91,41 @@ namespace TabletBot.Discord.Commands
                 }
             };
             embed.Description = issue.Body;
+            await message.Update(embed);
+        }
+
+        [Command("getartifacts", RunMode = RunMode.Async), Alias("artifacts", "artifact")]
+        public async Task GetArtifacts([Remainder] string url)
+        {
+            await Context.Message.DeleteAsync();
+            var message = await ReplyAsync("Fetching artifacts...");
+
+            string html;
+            using (var client = new HttpClient())
+                html = await client.GetStringAsync(url);
+
+            IEnumerable<Match> artifacts = await Task<MatchCollection>.Run(() => ArtifactRegex.Matches(html));
+            Match commitMatch = await Task<Match>.Run(() => CommitRegex.Match(html));
+            string sha = commitMatch.Groups["SHA"].Value;
+            string hash = string.Concat(sha.Take(7));
+            var commit = await GitHub.Git.Commit.Get(RepositoryOwner, RepositoryName, sha);
+            var title = commit.Message.Split(Environment.NewLine).First();
+            
+            var embed = new EmbedBuilder
+            {
+                Title = string.Format("{0} ({1})", title, hash),
+                Url = url,
+                Color = Color.Green
+            };
+
+            foreach (var artifact in artifacts)
+            {
+                var suite = Convert.ToInt32(artifact.Groups["Suite"].Value);
+                var id = Convert.ToInt32(artifact.Groups["Artifact"].Value);
+                var name = artifact.Groups["Name"].Value;
+                embed.AddField(name, string.Format("https://github.com/InfinityGhost/OpenTabletDriver/suites/{0}/artifacts/{1}", suite, id));
+            }
+
             await message.Update(embed);
         }
     }
