25aee738f1915ebbe95821975d73ba7eceeb5b55
InfinityGhost <infinity@opentabletdriver.net>
2022-10-09 17:08:40 -0400

    Check for multiple URLs in code block references

--- TabletBot.Discord/Watchers/GitHub/CodeMessageWatcher.cs
+++ TabletBot.Discord/Watchers/GitHub/CodeMessageWatcher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,44 +21,54 @@ namespace TabletBot.Discord.Watchers.GitHub
         }
 
         private static readonly Regex CodeRefRegex = new Regex(
-            @"https:\/\/github.com\/(?<Owner>.+?)\/(?<Repo>.+?)\/blob\/(?<GitRef>.+?)\/(?<Path>.+?)(\?.+?)?#L(?<StartLine>.+?)(?:-L(?<EndLine>.+?))?($| |>)",
+            @"https:\/\/github.com\/(?<Owner>.+?)\/(?<Repo>.+?)\/blob\/(?<GitRef>.+?)\/(?<Path>.+?)(?:\?.+?)?#L(?<StartLine>[0-9]+[0-9]?)(?:-L(?<EndLine>[0-9]+[0-9]?))",
             RegexOptions.Compiled
         );
 
         public async Task Receive(IMessage message)
         {
-            var match = CodeRefRegex.Match(message.Content);
-            if (message is IUserMessage userMessage && match.Success)
-            {
-                using (message.Channel.EnterTypingState())
-                {
-                    var owner = match.Groups["Owner"].Value;
-                    var repo = match.Groups["Repo"].Value;
-                    var gitRef = match.Groups["GitRef"].Value;
-                    var path = match.Groups["Path"].Value;
-                    var startLine = int.Parse(match.Groups["StartLine"].Value) - 1;
-                    var endLine = match.Groups["EndLine"].Success ? int.Parse(match.Groups["EndLine"].Value) : startLine + 1;
+            if (message is not IUserMessage userMessage)
+                return;
 
-                    var url = $"https://raw.githubusercontent.com/{owner}/{repo}/{gitRef}/{path}";
-                    var rawContent = await _client.Connection.GetHtml(new Uri(url));
+            var messageReference = userMessage.ToReference();
+            var blocks = GetCodeBlocks(message.Content);
 
-                    var fileContent = rawContent.Body.Split(Environment.NewLine);
-                    var lines = fileContent[startLine..endLine];
-
-                    var extension = Path.GetExtension(path).Replace(".", string.Empty);
-
-                    var sb = new StringBuilder();
-                    sb.AppendLine(Formatting.CodeString(path));
-                    sb.AppendCodeBlock(lines, extension);
-
-                    var text = sb.ToString();
-                    if (text.Length <= 2000)
-                        await message.Channel.SendMessageAsync(text, messageReference: userMessage.ToReference());
-                }
-            }
+            using (message.Channel.EnterTypingState())
+                await foreach (var block in blocks.Take(3))
+                    await message.Channel.SendMessageAsync(block, messageReference: messageReference);
         }
 
-        public Task Deleted(Cacheable<IMessage, ulong> message, Cacheable<IMessageChannel, ulong> channel) =>
-            Task.CompletedTask;
+        private async IAsyncEnumerable<string> GetCodeBlocks(string content)
+        {
+            foreach (Match match in CodeRefRegex.Matches(content))
+                yield return await GetFileContents(match);
+        }
+
+        private async Task<string> GetFileContents(Match match)
+        {
+            var owner = match.Groups["Owner"].Value;
+            var repo = match.Groups["Repo"].Value;
+            var gitRef = match.Groups["GitRef"].Value;
+            var path = match.Groups["Path"].Value;
+            var startLine = int.Parse(match.Groups["StartLine"].Value) - 1;
+            var endLine = match.Groups["EndLine"].Success ? int.Parse(match.Groups["EndLine"].Value) : startLine + 1;
+
+            var url = $"https://raw.githubusercontent.com/{owner}/{repo}/{gitRef}/{path}";
+            var rawContent = await _client.Connection.GetHtml(new Uri(url));
+                        
+            var fileContent = rawContent.Body.Split(Environment.NewLine);
+            var lines = fileContent[startLine..endLine];
+
+            var extension = Path.GetExtension(path).Replace(".", string.Empty);
+
+            var sb = new StringBuilder();            
+            sb.AppendLine(Formatting.CodeString(path));
+            sb.AppendCodeBlock(lines, extension);
+
+            // Clip to Discord's character limit of 2000
+            return sb.ToString(0, 2000);
+        }
+        
+        public Task Deleted(Cacheable<IMessage, ulong> message, Cacheable<IMessageChannel, ulong> channel) => Task.CompletedTask;
     }
 }
